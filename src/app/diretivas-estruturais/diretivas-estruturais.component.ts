fbfb8b106a200745f19d6ebd044b3ae158921cde
Luan Fernandes <hello@luanfernandes.dev>
2023-10-31 02:16:19 -0300

    ngFor

--- src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -8,6 +8,14 @@ import {Component, OnInit} from '@angular/core';
 export class DiretivasEstruturaisComponent implements OnInit {
   public condition: boolean = true;
   public conditionClick: boolean = true;
+  public items: Array<{ name: string, age: number }> = [
+    {name: 'Luan', age: 29},
+    {name: 'Heimy', age: 20},
+    {name: 'Safira', age: 1},
+    {name: 'Gordinho', age: 2},
+  ]
+
+  private _itemCount: number = this.items.length + 1;
 
 
   ngOnInit(): void {
@@ -22,4 +30,12 @@ export class DiretivasEstruturaisComponent implements OnInit {
     this.conditionClick = !this.conditionClick
   }
 
+  addItem(): void {
+    this.items.push({name: 'Added', age: this._itemCount})
+    this._itemCount++;
+  }
+
+  onClickEventList(event: number): void {
+    this.items.splice(event, 1)
+  }
 }
