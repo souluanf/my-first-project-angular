0b16a7319bce90ca367cef733e45706dd848e57f
Luan Fernandes <hello@luanfernandes.dev>
2023-11-12 19:44:47 -0300

    comunicação entre componentes: input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-first-project';
+  public addValue = 1;
+
+  public add() {
+    this.addValue++;
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewComponentComponent } from './new-component/new-component.component';
+import { InputComponent } from './input/input.component';
 
 
 
 @NgModule({
   declarations: [
-    NewComponentComponent
+    NewComponentComponent,
+    InputComponent
   ],
   exports: [
-    NewComponentComponent
+    NewComponentComponent,InputComponent
   ],
   imports: [
     CommonModule
